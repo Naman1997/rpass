a3ecb9b1d0f812c520e2ba976af8a9fce88a7c36
Naman1997 <aroranaman17@gmail.com>
2024-03-17 04:40:54 +1100

    Finally fix git clone with ssh key

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,51 @@
 #![deny(warnings)]
 
-use git2::{Error, Direction, Repository};
-use std::path::PathBuf;
+use std::env;
+use std::path::Path;
+use git2::build::{CheckoutBuilder, RepoBuilder};
+use git2::{Error, FetchOptions, RemoteCallbacks, Cred};
 
  
 fn run() -> Result<(), Error> {
     let home_dir = home::home_dir().expect("Unable to detect home dir!");
     let rpass_dir = "/.config/rpass";
     let full_path = format!("{}{}", home_dir.display(), rpass_dir);
-    let mut path = PathBuf::from(full_path);
 
-    let repo = match Repository::open(&path) {
-        Ok(repo) => {
-            print!("Using existing repo configured at ~/.config/rpass\n");
-            repo
-        },
-        Err(_e) => {
-            // Init the repo if it does not exist on that path
-            let repo = Repository::init(&path)?;
-            path = repo.workdir().unwrap().to_path_buf();
-            println!("Initialized empty Git repository in {}", path.display());
+    let repo_url = "git@github.com:Naman1997/My-notes.git";
 
-            create_initial_commit(&repo)?;
-            println!("Created empty initial commit");
-            repo
-        },
-    };
+    println!("Cloning {} into {}", repo_url, full_path);
 
-    // Check for exiting remotes and add if missing
-    let remote = "origin";
-    let mut remote = repo
-        .find_remote(remote)
-        .or_else(|_| repo.remote_anonymous(remote))?;
+    let mut callbacks = RemoteCallbacks::new();
+    let mut fetch_options = FetchOptions::new();
+    let checkout_options = CheckoutBuilder::new();
 
-    // Connect to the remote and call the printing function for each of the
-    // remote references.
-    let connection = remote.connect_auth(Direction::Fetch, None, None)?;
+    callbacks.credentials(|_, _, _| {
+        let credentials =
+            Cred::ssh_key(
+                "git",
+                None,
+                std::path::Path::new(&format!("{}/.ssh/id_ed25519", env::var("HOME").unwrap())),
+                None
+            ).expect("Could not create credentials object");
 
-    // Get the list of references on the remote and print out their name next to
-    // what they point to.
-    for head in connection.list()?.iter() {
-        println!("{}\t{}", head.oid(), head.name());
+
+        Ok(credentials)
+    });
+
+    fetch_options.remote_callbacks(callbacks);
+
+    if !Path::new("/etc/hosts").exists() {
+        RepoBuilder::new()
+        .fetch_options(fetch_options)
+        .with_checkout(checkout_options)
+        .clone(repo_url, Path::new(&full_path))?;
+
+        println!("Clone complete");
+    } else {
+        println!("Clone already exists");
     }
+    // Do things with `repo` here
 
-    Ok(())
-}
- 
-fn create_initial_commit(repo: &Repository) -> Result<(), Error> {
-    let sig = repo.signature()?;
-    let tree_id = {
-        let mut index = repo.index()?;
-        index.write_tree()?
-    };
-
-    let tree = repo.find_tree(tree_id)?;
-    repo.commit(Some("HEAD"), &sig, &sig, "Initial commit", &tree, &[])?;
     Ok(())
 }
 
