210dcf66642c89df6436717c2c74382f41fd3167
Naman1997 <aroranaman17@gmail.com>
2024-03-20 11:07:03 +1100

    Check for existing dir

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@ use std::path::Path;
 use git2::build::{CheckoutBuilder, RepoBuilder};
 use git2::{Error, FetchOptions, RemoteCallbacks, Cred};
 
- 
-fn run() -> Result<(), Error> {
-    let home_dir = home::home_dir().expect("Unable to detect home dir!");
-    let rpass_dir = "/.config/rpass";
-    let full_path = format!("{}{}", home_dir.display(), rpass_dir);
+const RPASS_DIR: &str = "/.config/rpass";
 
+ 
+fn run(full_path: &str) -> Result<(), Error> {
     let repo_url = "git@github.com:Naman1997/My-notes.git";
 
     println!("Cloning {} into {}", repo_url, full_path);
@@ -34,13 +32,14 @@ fn run() -> Result<(), Error> {
 
     fetch_options.remote_callbacks(callbacks);
 
-    if !Path::new(full_path.as_str()).exists() {
+    if !Path::new(full_path).exists() {
         RepoBuilder::new()
         .fetch_options(fetch_options)
         .with_checkout(checkout_options)
         .clone(repo_url, Path::new(&full_path))?;
 
         println!("Clone complete");
+        println!("rpass will now manage this dir and repo to commit your encrypted secrets!")
     } else {
         println!("Clone already exists");
     }
@@ -50,8 +49,20 @@ fn run() -> Result<(), Error> {
 }
 
 fn main() {
-    match run() {
-        Ok(()) => {}
-        Err(e) => println!("error: {}", e),
+    let home_dir = home::home_dir().expect("Unable to detect home dir!");
+    let full_path = format!("{}{}", home_dir.display(), RPASS_DIR);
+    let subcommand = std::env::args().nth(1).expect("No subcommand provided. #TODO replace with help section.");
+    if subcommand.eq_ignore_ascii_case("init") {
+        if !Path::new(&full_path).exists() {
+            match run(&full_path) {
+                Ok(()) => {}
+                Err(e) => println!("error: {}", e),
+            }
+        } else {
+            println!("rpass config dir exists. Backup and delete the dir manually to re-initiate a new config.")
+        }
+    } else if subcommand.eq_ignore_ascii_case("") {
+        
     }
+
 }
